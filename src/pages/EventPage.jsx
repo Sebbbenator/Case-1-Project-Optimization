d2ad266fec8d6123c8d11844bbb5fe1aaee93bb9
Sebbbenator <eaa25ssg@students.eaaa.dk>
2026-08-28 11:06:19 +0200

    tilføjet "alt" på billeder

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -102,7 +102,7 @@ export default function EventPage() {
         </Link>
 
         <section className="event-detail">
-          <img src={event.image} alt="" />
+          <img src={event.image} alt={event.title} />
           <div className="event-detail-content">
             <p className="event-category">{event.category}</p>
             <h1>{event.title}</h1>
