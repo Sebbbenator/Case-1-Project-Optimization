15634deef6f3d11cc79ae8f1355e3582996640d5
Sebbbenator <eaa25ssg@students.eaaa.dk>
2026-08-29 00:42:05 +0200

    lavet ens header på om siden

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
   return (
     <>
       <header
-        className="page-header about-header"
+        className="about-header"
         style={{
           backgroundImage:
             "url(https://images.unsplash.com/photo-1553376482-e96b68bd1e11?q=80&w=2400&auto=format&fit=crop&ixlib=rb-4.1.0)"
