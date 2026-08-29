9845817b6f0c584748e718a1e91e7444c803f9dc
Sebbbenator <eaa25ssg@students.eaaa.dk>
2026-08-29 12:56:27 +0200

    billede bliver hentet frra supabase nu

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -111,13 +111,7 @@ export default function EventPage() {
   return (
     <>
       <header className="event-hero">
-        <div
-          className="event-hero-photo"
-          style={{
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1595146463222-19603449c6af?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"
-          }}
-        >
+        <div className="event-hero-photo" style={{ backgroundImage: `url(${event.image})` }}>
           <Link className="back-link" to="/events">
             ← Alle events
           </Link>
